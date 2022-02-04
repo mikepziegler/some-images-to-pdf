cea373532aa65a9e773ce0602e2f0c87bbaf072f
mikezyeman <mike.zyeman@gmail.com>
2022-02-04 21:14:37 +0100

    Finished first beta of cli. Functionality implemented. Implemented directory reader and pdf generator with scaled images.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Some Images to pdf
+
+A npm cli tool for convertion multiple pictures into one pdf
+
+## How to use it?
+
+To use the package open up your terminal, cd to the directory with your images and enter
+
+```npx some-images-to-pdf```
+
+It will copy all images into one pdf file.
---
+++ src/index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {jsPDF} from 'jspdf';
+import imageToBase64 from "image-to-base64/image-to-base64.js";
+import chalk from "chalk";
+import {createSpinner} from "nanospinner";
+
+import sizeOf from 'image-size';
+
+const imageExtensions = [
+    '.jpg', '.jpeg',
+    '.png', '.svg'
+]
+
+const A4Properties = {
+    width: 210,
+    height: 297
+}
+
+async function convertImagesToPDF(name = 'generated') {
+
+    console.log(chalk.yellowBright("Some images to one pdf"));
+    const spinner = createSpinner('Processing pictures...').start();
+
+    fs.readdir(process.cwd(), async (err, files) => {
+
+        if (err) {
+            spinner.error({text: err});
+            spinner.stop();
+            throw new Error(err);
+        }
+
+        const images = files.filter(file => imageExtensions.includes(path.extname(file)))
+        if (images.length === 0) {
+            spinner.error({text: ''})
+            spinner.stop();
+            throw new Error(`No images in folder. Be sure that folder contains images/ files with those extensions: ${imageExtensions.join(' ')} `);
+        }
+        const pdf = new jsPDF();
+
+        for (let [index, image] of images.entries()) {
+
+            if (index > 0) pdf.addPage()
+
+            const ext = path.extname(image).substring(1);
+            const base64 = await imageToBase64(image);
+
+            const dimensions = await sizeOf(image);
+            console.log(dimensions);
+
+            const ratio = dimensions.height / dimensions.width;
+            let width = A4Properties.width,
+                height = ratio * A4Properties.width;
+
+            console.log(width, height);
+
+            pdf.addImage(base64, ext, 0, 0, width, height);
+
+        }
+
+        pdf.save(name + '.pdf')
+        spinner.success({text: `PDF has been successfully generated.`});
+    })
+}
+
+await convertImagesToPDF()
+    .catch((err) => {
+        console.log(err)
+    })
